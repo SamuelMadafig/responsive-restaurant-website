086010a4e8ab7194930013dbf9fe0525aa7de461
SamuelMadafig <35886695+SamuelMadafig@users.noreply.github.com>
2019-01-18 15:24:52 +1100

    uploading files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+    document.querySelector(".nav-bar__icon").addEventListener('click',()=>{
+        document.querySelector('.nav-bar__items').classList.toggle('icon-toggle');
+        document.querySelector('.nav-bar').classList.toggle('hightFit')
+    })
+   
